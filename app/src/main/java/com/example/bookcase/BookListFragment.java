7980a1e5fa8d571c78627f802bdeda179218db55
Kwamina Thompson <tug82420@temple.edu>
2019-03-22 18:41:22 -0400

    Array of Strings created in resources

--- app/src/main/java/com/example/bookcase/BookListFragment.java
+++ app/src/main/java/com/example/bookcase/BookListFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
@@ -19,6 +22,10 @@ import android.view.ViewGroup;
  */
 public class BookListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
+
+    ListView listView;
+
+    Context context;
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -91,16 +98,7 @@ public class BookListFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
